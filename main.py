5df5aacdb212c670a00a74f43f919e60fbf571d6
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2026-05-12 13:14:45 +0300

    Connect with db and front

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:3000")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+supabase: Client = create_client(
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_KEY"),
+)
 
 
 @app.get("/")
