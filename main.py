8fe403dccffb39d110074e7728270b6946b45d75
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2026-05-13 10:57:12 +0300

    Cors issue

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:3000")],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://careercompass.etijahcoaching.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
